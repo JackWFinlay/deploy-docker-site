5f9b8357469c3475c2ed44224a3c8457ad92ceb4
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-04-11 21:32:51 +1200

    Create Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM nginx
+
+COPY ../JackWFinlay.github.io /usr/share/nginx/html
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/JackWFinlay/JackWFinlay.github.io.git
+
+mkdir /deploy
+cp Dockerfile /deploy
+cd /deploy
+
+docker build -t nginxserverimage .
+docker run --name nginxserver -P -d nginxserverimage
+
+
+
+
+
