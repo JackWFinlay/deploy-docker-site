a8d13f7b851d91b8739d392dd24a3091aa24cad5
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-04-12 20:05:50 +1200

    Add docker-compose.yml

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM nginx
-
-COPY JackWFinlay.github.io /usr/share/nginx/html
--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 mkdir ~/deploy
-cp Dockerfile ~/deploy
+cp deployment-assets/* ~/deploy
 cd ~/deploy
 
 git clone https://github.com/JackWFinlay/JackWFinlay.github.io.git
+git clone https://github.com/JackWFinlay/Blog-Engine.git
+
+# docker-compose up
+
+
+# rm nginx-conf/default.conf
+# cp nginx.conf nginx-conf
+# cp default nginx-conf/sites-available
 
 docker build -t nginxserverimage .
 docker run --name nginxserver -P -d nginxserverimage
-
-
-
-
-
