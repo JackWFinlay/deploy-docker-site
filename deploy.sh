18da83a5ad2b46738f9556e21b672631adf1aff4
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-04-12 20:11:15 +1200

    Update deploy.sh to use docker-compose

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,11 @@ cd ~/deploy
 git clone https://github.com/JackWFinlay/JackWFinlay.github.io.git
 git clone https://github.com/JackWFinlay/Blog-Engine.git
 
-# docker-compose up
-
+docker-compose up
 
 # rm nginx-conf/default.conf
 # cp nginx.conf nginx-conf
 # cp default nginx-conf/sites-available
 
-docker build -t nginxserverimage .
-docker run --name nginxserver -P -d nginxserverimage
+# docker build -t nginxserverimage .
+# docker run --name nginxserver -P -d nginxserverimage
