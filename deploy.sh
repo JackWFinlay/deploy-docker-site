45edcdd89db31617a08d0e704a0d2a906e431e67
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-04-12 21:21:15 +1200

    Fix

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,15 @@ git clone https://github.com/JackWFinlay/Blog-Engine.git
 docker-compose up
 
 # rm nginx-conf/default.conf
-cp challenge.conf nginx-conf
+cp letsencryptchallenge/app.conf nginx-conf
 
 docker-compose run --rm letsencrypt \
   letsencrypt certonly --webroot \
   --email jack@jackfinlay.com --agree-tos \
   -w /var/www/letsencrypt -d depthcharge.co
 
-docker-compose kill -s SIGHUP proxy
-
 cp app.conf nginx-conf
+docker-compose kill -s SIGHUP proxy
 
 # docker build -t nginxserverimage .
 # docker run --name nginxserver -P -d nginxserverimage
