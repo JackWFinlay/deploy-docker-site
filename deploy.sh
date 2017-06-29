5334bcc89fa5c8812e3dd55724f0e8aad84b924d
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-06-29 18:55:26 +1200

    Updates

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,14 @@ cd ~/deploy
 git clone https://github.com/JackWFinlay/Depthcharge.Render.git
 git clone https://github.com/JackWFinlay/Jsonize-Web.git
 
-docker-compose build
-docker-compose run dotnet-site dotnet restore
+# docker-compose run proxy -d
 
-docker-compose run proxy -d
-
-#docker-compose run --rm letsencrypt \
+# docker-compose run --rm letsencrypt \
 #  certonly --webroot \
 #  --email jack@jackfinlay.com --agree-tos \
 #  -w /var/www/letsencrypt -d depthcharge.co
 
 docker-compose up -d
+
 cp ./default.conf nginx-conf
 docker-compose restart proxy
